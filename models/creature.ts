3e3e87aa9cd73e03ee2cde561837a46d14766f16
Sebastian Toporsch <sebsn@gmx.at>
2025-03-16 22:24:51 +0100

    Added fields to model

--- models/creature.ts
+++ models/creature.ts
@@ -1,17 +1,36 @@
 export type Creature = {
   [key: string]: unknown;
   id: string;
-  url: string;
-  name: string;
-  level: number;
   ac: string;
-  hp: string;
   alignment: string;
-  creature_family_markdown: string;
-  size: string[];
-  trait_markdown: string[];
+  charisma: number;
+  constitution: number;
   count: number;
   challenge_type: ChallengeType;
+  creature_familiy: string;
+  creature_family_markdown: string;
+  dexterity: string;
+  fortitude_save: number;
+  hp: string;
+  intelligence: number;
+  level: number;
+  name: string;
+  reflex_save: number;
+  resistance_raw: string;
+  size: string[];
+  skill_mod: NumberObject;
+  speed_raw: string;
+  strength: string;
+  trait: string;
+  trait_markdown: string[];
+  url: string;
+  wisdom: number;
+  weakness_raw: string;
+  will_save: number;
+};
+
+type NumberObject = {
+  [key: string]: number;
 };
 
 export type ChallengeType = "weak" | "base" | "elite";
