c3d37de94043eec40e984e3a1e803ff46ef7ff60
Sebastian Toporsch <sebsn@gmx.at>
2025-04-16 23:34:50 +0200

    Added resolve aliases for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,15 @@
 import { defineVitestConfig } from "@nuxt/test-utils/config";
+import path from "path";
 
 export default defineVitestConfig({
   test: {
     globals: true,
     environment: "happy-dom",
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./"),
+      "@": path.resolve(__dirname, "./"),
+    },
   },
 });
