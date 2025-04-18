91eefd1fbd0ef44a06ceb44c4dfb7a024e04b4c9
Sebastian Toporsch <sebsn@gmx.at>
2025-04-18 16:44:16 +0200

    Removed home button for now

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,11 +1,6 @@
 <template>
   <header class="header">
     <h1>PathFighter</h1>
-    <nav>
-      <ul>
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-      </ul>
-    </nav>
     <OptionToolbar />
   </header>
 </template>
